9b34851c30b0f545a569efdc2ce1c43f4c298424
Anti-k <romanslyzebniy@gmail.com>
2016-07-01 00:07:38 +0300

    index to question id added

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of :body }
-  it { should belong_to :question}
+  it { should belong_to :question }
+  it { should have_db_index :question_id }
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }              
   it { should validate_presence_of :body } 
   it { should have_db_index :title }
-  it { should have_many :answers }
+  it { should have_many :answers }  
 
 end
