8e2c82240f42c869fa4b9614d42c876a0a7be56d
Anti-k <romanslyzebniy@gmail.com>
2016-06-30 19:06:35 +0300

    standart RSpec to Question added

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  it 'validates presence of title' do
+    expect(Question.new(body:'123')).to_not be_valid    
+  end
+  it 'validates presence of body' do
+    expect(Question.new(title:'123')).to_not be_valid
+  end
+end
