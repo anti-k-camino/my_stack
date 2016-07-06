7a0baed204ae9dc969f606cf6994c22f0985903a
Anti-k <romanslyzebniy@gmail.com>
2016-07-06 21:44:05 +0300

    user gets the list of questions

---
+++ spec/acceptance/view_questions_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+feature 'User gets list of questions', %q{
+  In order to be able to answer a question
+  Or on order to get an answer for existiong question
+  As an unregistered user 
+  I want to be able to see list of existing questions
+} do   
+  scenario 'Unregistered user try to get the list of questions' do
+    visit questions_path
+    #expect(page).to have_current_path questions_path    
+    expect(page).to have_selector 'ul'
+  end
+end
