ca5b9becdfeeec26cc015a3b21f97ff3547f8374
Anti-k <romanslyzebniy@gmail.com>
2016-07-08 11:56:48 +0300

    refactored

--- spec/acceptance/give_answer_spec.rb
+++ spec/acceptance/give_answer_spec.rb
@@ -6,16 +6,24 @@ feature 'Give an answer', %q{
   I want to give an answer on that question
 } do 
   given(:question){ create :question }
-  scenario 'User creates an answer to a question' do
+  before do
     question
     visit questions_path    
-    click_on "#{question.title}"
+    click_on "#{ question.title }"
     expect(current_path).to eq question_path question    
     click_on 'Answer'
+  end
+  scenario 'User creates an answer to a question' do   
     fill_in 'Body', with:'text text'
     click_on 'Create answer'   
     expect(current_path).to eq question_path question
-    expect(page).to have_content('Your answer successfully added.')    
-    expect(page).to have_content("#{ question.answers.last.body }")
+    expect(page).to have_content 'Your answer successfully added.'    
+    expect(page).to have_content "#{ question.answers.last.body }"
   end
+  scenario 'User try to create a question with empty body' do    
+    click_on 'Create answer'
+    expect(current_path).to eq question_answers_path question
+    expect(page).to have_content 'error'
+  end
+
 end
