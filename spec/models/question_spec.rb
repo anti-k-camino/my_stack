201935bd8655db4f15ffc3a1402d148a3a590597
Anti-k <romanslyzebniy@gmail.com>
2016-07-09 21:02:22 +0300

    permission? method is tested

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :user_id }
   it { should have_db_index :title }
   it { should have_db_index :user_id }
-  it { should have_many(:answers).dependent(:destroy) }   
+  it { should have_many(:answers).dependent :destroy }  
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,20 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :name } 
   it { should validate_uniqueness_of(:name).case_insensitive } 
   it { should have_db_index :name }
-  it { should have_many(:answers).dependent(:destroy) } 
-  it { should have_many(:questions).dependent(:destroy) }
+  it { should have_many(:answers).dependent :destroy } 
+  it { should have_many(:questions).dependent :destroy }
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+  let(:user){ create :user } 
+  let(:user1){ create :user }
+  let(:question){ create :question, user: user }
+  let(:question1){ create :question, user: user1}
+
+  it "returns true when user is the author || false when it is not" do    
+    user.permission?(question).should == true
+    user1.permission?(question).should == false
+    user.permission?(question1).should == false
+    user1.permission?(question1).should == true
+  end
+
 end
