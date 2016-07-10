da3c429ee92f09048dbc0e08e9b5f92c23891f33
Anti-k <romanslyzebniy@gmail.com>
2016-07-10 22:24:26 +0300

    asiinging by id is chaged

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,7 +24,7 @@ class QuestionsController < BaseController
   end
   def create
     @question = Question.new(question_params)
-    @question.user_id = current_user.id
+    @question.user = current_user
     if @question.save
       redirect_to @question, notice: 'Your question successfully created.'
     else
