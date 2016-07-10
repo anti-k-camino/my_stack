e91b7fffc2a8a6793fdfe3ba5d2dbbb2015be895
Anti-k <romanslyzebniy@gmail.com>
2016-07-10 13:07:03 +0300

    question delete tested

--- spec/acceptance/delete_question_spec.rb
+++ spec/acceptance/delete_question_spec.rb
@@ -10,16 +10,17 @@ feature 'User can delete a question', %q{
     given(:malicious_user){ create :user }
     given(:question){ create :question, user: user }    
     
-    scenario 'Author of a question deletes a question' do 
-      visit root_path    
+    scenario 'Author of a question deletes a question' do            
       sign_in user         
-      visit question_path question      
-      expect(page).to have_content 'Delete'
-      click_on 'Delete'      
+      visit question_path question
+      within("//p[class='question']"){ click_on 'Delete' }             
       expect(page).to have_content 'Question successfully destroyed.'
       expect(current_path).to eq questions_path      
     end
-    scenario 'Not th author of a question try to delete a question' do
+    scenario 'Not the author of a question try to delete a question' do
+      sign_in malicious_user
+      visit question_path question       
+      within("//p[class='question']"){ expect(page).to_not have_content 'Delete' }              
     end
   end
   scenario 'Non authenticated user wants to delete a question' do
