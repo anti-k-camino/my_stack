3f69f8c4f735d64bc8e88fb16e93015b40906913
Anti-k <romanslyzebniy@gmail.com>
2016-07-11 15:15:50 +0300

    pendings are added

--- spec/acceptance/give_answer_spec.rb
+++ spec/acceptance/give_answer_spec.rb
@@ -13,6 +13,7 @@ feature 'Give an answer', %q{
     expect(current_path).to eq question_path question    
     click_on 'Answer'
   end
+  
   scenario 'User creates an answer to a question' do   
     fill_in 'Body', with:'text text'
     click_on 'Create answer'   
@@ -20,6 +21,7 @@ feature 'Give an answer', %q{
     expect(page).to have_content 'Your answer successfully added.'    
     expect(page).to have_content "#{ question.answers.last.body }"    
   end
+
   scenario 'User try to create a question with empty body' do    
     click_on 'Create answer'
     expect(current_path).to eq question_answers_path question
