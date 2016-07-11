2dba2d56cb5641a74307448bc127573a2f850c16
Anti-k <romanslyzebniy@gmail.com>
2016-07-11 12:54:49 +0300

    Non authenticated user is added

--- spec/acceptance/delete_question_spec.rb
+++ spec/acceptance/delete_question_spec.rb
@@ -4,25 +4,30 @@ feature 'User can delete a question', %q{
   In order to manage my publications
   As an authenticated user and author of a question
   I want to delete a question
-} do
-  context 'Athenticated user wants to delete a question' do
-    given(:user){ create :user }
-    given(:malicious_user){ create :user }
-    given(:question){ create :question, user: user }    
-    
+} do 
+  given(:user){ create :user } 
+  given(:question){ create :question, user: user }
+  context 'Athenticated user wants to delete a question' do    
+    given(:malicious_user){ create :user }      
     scenario 'Author of a question deletes a question' do            
       sign_in user         
       visit question_path question
-      within("//p[class='question']"){ click_on 'Delete' }             
+      @body = question.body
+      within("div.question_body"){ click_on 'Delete' }             
       expect(page).to have_content 'Question successfully destroyed.'
-      expect(current_path).to eq questions_path      
+      expect(current_path).to eq questions_path 
+      expect(page).to_not have_content "#{@body}"     
     end
     scenario 'Not the author of a question try to delete a question' do
       sign_in malicious_user
       visit question_path question       
-      within("//p[class='question']"){ expect(page).to_not have_content 'Delete' }              
+      within("div.question_body"){ expect(page).to_not have_content 'Delete' }              
     end
   end
-  scenario 'Non authenticated user wants to delete a question' do
+  context 'Non authenticated user wants to delete a question' do
+    scenario 'No authenticated user can not destroy a question' do
+      visit question_path question      
+      expect(page).to_not have_content 'Delete'
+    end
   end
 end
