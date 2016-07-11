2c80d86645c8171e5dcb036ffccb2e1d5ebd2b05
Anti-k <romanslyzebniy@gmail.com>
2016-07-11 12:29:30 +0300

    creating question refactored

--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -15,11 +15,19 @@ feature 'create question', %q{
     fill_in 'Title', with:'test question'
     fill_in 'Body', with:'text text'
     click_on 'Create'
-    expect(page).to have_content 'Your question successfully created.'        
+    expect(page).to have_content 'Your question successfully created.'    
+    within('div.question_title') do
+      expect(page).to have_content 'test question'
+    end
+    within('div.question_body') do
+     expect(page).to have_content 'text text'
+     expect(page).to have_link 'Delete'
+    end       
   end
   scenario 'Non authenticated user tries to create a question' do
     visit questions_path
     click_on 'Ask question'
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'    
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    expect(current_path).to eq new_user_session_path    
   end
 end
