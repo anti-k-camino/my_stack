f2d1650b0f3f68e1f92b90030436b54787cf1fb1
Anti-k <romanslyzebniy@gmail.com>
2016-07-11 11:57:42 +0300

    adding answers is moved to question show page

--- spec/acceptance/give_answer_to_question_spec.rb
+++ spec/acceptance/give_answer_to_question_spec.rb
@@ -3,7 +3,35 @@ feature 'answer aquestion', %q{
   As an authenticated User
   I want to give an answer to a question
 } do
-  scenario 'Authenticated user answers a question' do
-    
+  given!(:question){ create :question } 
+  given(:user){ create :user }  
+  context 'Authenticated user creates answer' do
+    scenario 'Authenticated whants to create a question' do
+      sign_in user 
+      visit question_path question            
+      within('div.create_answer') do  
+        expect(page).to have_button 'Create answer' 
+      end   
+      fill_in 'Body', with: 'example answer'    
+      click_on 'Create answer'
+      expect(current_path).to eq question_path question
+      expect(page).to have_content 'Your answer successfully added.'
+      expect(page).to have_content 'example answer'   
+    end
+  end
+  context 'Non authenticated user creates question' do
+    given(:not_auth_user){ create :user }
+    scenario 'Non authenticated user whants to create an answer' do
+      visit question_path question           
+      within('div.create_answer') do  
+        expect(page).to have_button 'Create answer' 
+      end   
+      fill_in 'Body', with: 'example answer'    
+      click_on 'Create answer'
+      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+      expect(current_path).to eq new_user_session_path 
+      visit question_path question
+      expect(page).to_not have_content 'example answer'
+    end
   end
 end
