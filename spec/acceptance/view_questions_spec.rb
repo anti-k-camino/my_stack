8deda813045a39e4eb363d168e7405e2910d6b90
Anti-k <romanslyzebniy@gmail.com>
2016-07-12 11:33:44 +0300

    expect on questions titles are added

--- spec/acceptance/view_questions_spec.rb
+++ spec/acceptance/view_questions_spec.rb
@@ -4,10 +4,13 @@ feature 'User gets list of questions', %q{
   Or on order to get an answer for existiong question
   As an unregistered user 
   I want to be able to see list of existing questions
-} do   
+} do 
+  let!(:questions){ create_list(:question, 2)}   
   scenario 'Unregistered user try to get the list of questions' do
     visit questions_path
     expect(page).to have_current_path questions_path    
     expect(page).to have_selector 'ul'
-  end
+    expect(page).to have_content "#{ questions[0].title }"    
+    expect(page).to have_content "#{ questions[1].title }"
+  end 
 end
