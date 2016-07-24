1d3131a10e17359a4a6dbbd4d85cf1ed5862141b
Anti-k <romanslyzebniy@gmail.com>
2016-07-24 17:40:35 +0300

    Plymorphic attachments, Answers associated with attachments

---
+++ spec/acceptance/answer/add_files_to_answers_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../acceptance_helper'
+feature 'Add files to answer', %q{
+  In order to iluustrate my answer
+  As an answer author
+  I would like to be able to attach files
+} do 
+  given(:user){ create(:user) }
+  given(:question){ create(:question) } 
+
+  background do
+    sign_in user
+    visit question_path(question)
+  end
+  scenario 'User adds file when gives an answer' do
+    within('div.create_answer') do
+      fill_in 'Body', with: 'answer body'
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+      expect(page).to have_link "spec_helper.rb", href: "/uploads/attachment/file/1/spec_helper.rb"
+    end
+  end
+end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -8,6 +8,8 @@ RSpec.describe Answer, type: :model do
   it { should have_db_index :question_id }
   it { should belong_to :user }  
   it { should have_db_index :user_id }
+  it { should have_many :attachments }
+  it { should accept_nested_attributes_for :attachments }
 
   describe 'best!' do    
     let!(:question){ create :question }
