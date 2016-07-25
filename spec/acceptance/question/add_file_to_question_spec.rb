338b583dd60662a763efcfa3a6048f644df70dcb
Anti-k <romanslyzebniy@gmail.com>
2016-07-25 11:32:39 +0300

    spec on removing file while creating a question is added

--- spec/acceptance/question/add_file_to_question_spec.rb
+++ spec/acceptance/question/add_file_to_question_spec.rb
@@ -15,26 +15,46 @@ feature 'Add files to question', %q{
     fill_in "Body", with: "text text"
   end
 
-  scenario 'User adds file when asks question' do    
+  scenario 'User adds file when asks question', js: true do    
     attach_file "File", "#{ Rails.root }/spec/spec_helper.rb"
     click_on "Create"
-
     expect(page).to have_link "spec_helper.rb", href:"/uploads/attachment/file/1/spec_helper.rb"
   end
 
   scenario 'User adds several files to question', js: true do    
     click_on "add file"
-    files = all("input[type='file']")
+    wait_for_ajax
+    files = page.all("input[type='file']")
       
     files[0].set("#{ Rails.root }/spec/spec_helper.rb")
     files[1].set("#{ Rails.root }/spec/rails_helper.rb")
-     
-      
-    
-    
+    click_on "Create"    
+        
+    expect(page).to have_link "spec_helper.rb", href:"/uploads/attachment/file/1/spec_helper.rb"
+    expect(page).to have_link "rails_helper.rb", href:"/uploads/attachment/file/2/rails_helper.rb"
+  end
+
+  scenario "Blank files are not added", js: true do
+    click_on "add file"
+    click_on "add file"
+    wait_for_ajax
+    files = page.all("input[type='file']")
+    files[0].set("#{ Rails.root }/spec/spec_helper.rb")
+    files[2].set("#{ Rails.root }/spec/rails_helper.rb")
+    click_on "Create"    
+    expect(page).to have_link "spec_helper.rb", href:"/uploads/attachment/file/1/spec_helper.rb"
+    expect(page).to have_link "rails_helper.rb", href:"/uploads/attachment/file/2/rails_helper.rb"
+  end
+
+  scenario "Can remove files on creating a question", js: true do
+    click_on "add file"    
+    wait_for_ajax
+    files = page.all("input[type='file']")
+    files[0].set("#{ Rails.root }/spec/spec_helper.rb")
+    files[1].set("#{ Rails.root }/spec/rails_helper.rb")
+    first(".nested-fields").click_on "remove file"
     click_on "Create"
-    save_and_open_page     
-    expect(page).to have_link "spec_helper.rb", href:"/uploads/attachment/file/2/spec_helper.rb"
-    expect(page).to have_link "rails_helper.rb", href:"/uploads/attachment/file/3/rails_helper.rb"
+    expect(page).to_not have_link "spec_helper.rb", href:"/uploads/attachment/file/1/spec_helper.rb"
+    expect(page).to have_link "rails_helper.rb", href:"/uploads/attachment/file/1/rails_helper.rb"
   end
 end
