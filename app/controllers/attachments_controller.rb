47ff2d0794236e9f5c7be31e8c2397d66353e130
Anti-k <romanslyzebniy@gmail.com>
2016-07-26 12:24:20 +0300

    find is replaced with polymorphic association

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,8 +1,7 @@
 class AttachmentsController < ApplicationController
   def destroy
-    @attachment = Attachment.find(params[:id])
-    @res = @attachment.attachable_type.constantize
-    unless current_user.author_of?(@res.find(@attachment.attachable_id))   
+    @attachment = Attachment.find(params[:id])    
+    unless current_user.author_of?(@attachment.attachable)   
       redirect_to questions_path , notice: 'Restricted' and return
     end    
     @attachment.destroy     
