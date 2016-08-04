90e2ef187b9d6d672ace941485590b995d6baf37
Anti-k <romanslyzebniy@gmail.com>
2016-08-04 14:29:11 +0300

    voting from model is extacted to concern

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,23 +1,14 @@
 class Question < ActiveRecord::Base 
   
   include HasUser
+  include Votings
 
   has_many :answers, dependent: :destroy 
   has_many :attachments, as: :attachable, dependent: :destroy  
-  has_many :votes, as: :votable, dependent: :destroy
-  has_many :users, through: :votes
   
   validates :title, :body, presence: true  
 
   accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
-
-  def check_if_voted? user
-    self.users.include? user
-  end
-
-  def get_vote user
-    self.votes.where(user: user).first
-  end
 
   def rating
     self.votes.sum(:vote_field)  
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Vote, type: :model do
   it { should validate_presence_of :user_id } 
   it { should validate_presence_of :votable }
   it { should validate_presence_of :vote_field } 
-  it { should have_db_index [:user_id, :votable_id, :votable_type] }
+  it { validate_uniqueness_of(:user_id).scoped_to([:votable_id, :votable_type])}
+  it { should have_db_index [:user_id, :votable_id, :votable_type] }  
 end
