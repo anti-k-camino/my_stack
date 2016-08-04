9dfaa566b1a7cde2da08abbc3aa6ab223f0992ac
Anti-k <romanslyzebniy@gmail.com>
2016-08-04 10:58:52 +0300

    bug with voting fixed

--- spec/factories/votes.rb
+++ spec/factories/votes.rb
@@ -1,18 +1,4 @@
 FactoryGirl.define do 
-
- 
-  factory :vote_questions, class:'Vote' do
-    votable_id 1
-    votable_type "Question"
-    user_id 1
-    vote_field true
+  factory :vote  do  
   end
-
-  factory :vote_answers, class:'Vote' do
-    votable_id 
-    votable_type "Answer"
-    user_id 
-    vote_field true
-  end
-
 end
