418dc1fae961fc9bb287316291c7725541ad91bf
Anti-k <romanslyzebniy@gmail.com>
2016-08-07 00:32:40 +0300

    leftover file removed

--- app/models/permission_validator.rb
+++
@@ -1,7 +0,0 @@
-class PermissionValidator < ActiveModel::Validator
-  def validate(obj)
-    if obj.user == obj.votable.user 
-      obj.errors[:user] << "Permission denied for author of resource" 
-    end   
-  end
-end
