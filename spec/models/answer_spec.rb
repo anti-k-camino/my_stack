4af997a96f257ecf8b852d3db1d6c96db00c2538
Anti-k <romanslyzebniy@gmail.com>
2016-08-07 11:56:33 +0300

    answer method tested

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -28,4 +28,18 @@ RSpec.describe Answer, type: :model do
       expect(best_answer).to_not be_best
     end
   end
+
+  describe 'check_if_voted?' do 
+    let!(:user){ create :user } 
+    let!(:sample_user){ create :user }  
+    let!(:answer){ create :answer }
+    let!(:vote){ create :vote, user: user, votable: answer, vote_field: 1}   
+  
+    it 'user different from resource user is accapteble' do
+      expect(answer.check_if_voted?(sample_user)).to be_falsy
+    end
+    it 'user similar to resource user is accapteble' do
+      expect(answer.check_if_voted?(user)).to be_truthy
+    end
+  end
 end
