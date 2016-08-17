3e7b1fd72cdeba3c4f07bdaa00afd89d585f8848
Anti-k <romanslyzebniy@gmail.com>
2016-08-17 19:46:11 +0300

    coment controller is tested

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -11,14 +11,30 @@ RSpec.describe CommentsController, type: :controller do
         it 'changes question comments quantity' do       
           expect{ post :create, comment:{body: 'test'}, question_id: question, format: :json}.to change(question.comments, :count).by 1
         end
+        it 'response with propper json' do
+          post :create, comment:{body: 'test'}, question_id: question, format: :json
+          expect(JSON.parse(response.body)["comment"]["body"]).to eq "test"
+          expect(JSON.parse(response.body)["user_name"]).to eq @user.name
+        end
       end
 
       context 'invalid attributes' do
         it 'does not change question comments quantity' do
           expect{ post :create, comment:{ user: @user }, question_id: question, format: :json }.to_not change(question.comments, :count)
         end
+        it 'responds with error' do
+          post :create, comment:{ body: ""}, question_id: question, format: :json
+          expect(JSON.parse(response.body)["errors"][0]).to eq "Body can't be blank"
+        end
       end
 
+    end
+
+    context 'Non authenticated user' do
+      let!(:question){ create :question }
+      it 'does not create a comment' do
+        expect{ post :create, comment:{ body: "body"}, question_id: question, format: :json }.to_not change(question.comments, :count)
+      end
     end
   end
 end
