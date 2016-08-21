7ce4950d4b0db753bdef1c7ce38ec68cfd596846
Anti-k <romanslyzebniy@gmail.com>
2016-08-21 10:17:48 +0300

    pubsub for adding questions is set

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -32,6 +32,7 @@ class QuestionsController < ApplicationController
     @question = Question.new(question_params)
     @question.user = current_user
     if @question.save
+      PrivatePub.publish_to '/questions', question: @question.to_json
       redirect_to @question, notice: 'Your question successfully created.'
     else
       render :new
