ab36afde43db2705677e1fd6884943cf8effcbe5
Anti-k <romanslyzebniy@gmail.com>
2016-09-07 18:02:42 +0300

    initial commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,7 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-# Add additional requires below this line. Rails is not loaded until this point!
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
