0619398f8386bb6b05895aa9a185c28f58ae6c6d
Anti-k <romanslyzebniy@gmail.com>
2016-09-09 11:44:26 +0300

    specs completed

--- spec/controllers/authorizations_controller_spec.rb
+++ spec/controllers/authorizations_controller_spec.rb
@@ -53,6 +53,44 @@ RSpec.describe AuthorizationsController, type: :controller do
       it "creates a new authorization for user" do
         expect{ post :create, authorization: { email: "example@gmail.com" }}.to change(user.authorizations, :count).by 1
       end
+
+      it "should redirect to root_path" do
+        post :create, authorization: { email: "example@gmail.com" }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "user does not exist" do
+      before do
+        auth_hash = {
+            provider: 'twitter',
+            uid: '1234567',
+            info: { name: 'SomeName' }
+        }
+        session['devise.provider_data'] = OmniAuth::AuthHash.new(auth_hash)
+      end
+
+      it "should create a new user" do
+        expect{ post :create, authorization: { email: "example@gmail.com" }}.to change(User, :count).by 1
+      end
+
+      it "should create an authorization for user" do
+        post :create, authorization: { email: "example@gmail.com" }
+        user = User.find_by_email("example@gmail.com")
+        expect(user.authorizations.count).to eq 1
+      end
+
+      it "should create a propper authorization for user" do
+        post :create, authorization: { email: "example@gmail.com" }
+        user = User.find_by_email("example@gmail.com")
+        expect(user.authorizations.first.uid).to eq "1234567"
+      end
+
+      it "should redirect to root path" do
+        post :create, authorization: { email: "example@gmail.com" }
+        expect(response).to redirect_to root_path
+      end
+
     end
 
   end
