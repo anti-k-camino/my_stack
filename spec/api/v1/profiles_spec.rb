e925e329a745734f028d84c416a4c7b145109cd2
Anti-k <romanslyzebniy@gmail.com>
2016-09-14 09:45:47 +0300

    specs refactored

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -40,50 +40,48 @@ describe 'Profile API' do
 
   
   describe 'GET /index' do
-    context 'authorized' do
-      let!(:users){ create_list :user, 3 }
-      
-      
-      let(:access_token) { create(:access_token, resource_owner_id: users[0].id) }
-      
-
-      before do        
-        get '/api/v1/profiles', format: :json, access_token: access_token.token
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access token' do
+        get '/api/v1/profiles', format: :json
+        expect(response.status).to eq 401
       end
 
-      it 'return 200 status' do
+      it 'returns 401 status if access token is invalid' do
+        get '/api/v1/profiles', format: :json, access_token: '123456'
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let!(:users) { create_list(:user, 4) }
+      let(:access_token) { create(:access_token, resource_owner_id: users[0].id) }
+
+      before { get '/api/v1/profiles', format: :json, access_token: access_token.token }
+
+      it 'returns 200 status' do
         expect(response).to be_success
       end
-
 
       it 'not contain current_resource_owner' do
         expect(response.body).to_not include_json(users[0].to_json)
       end
 
-
-      it "return array of user objects propper length" do        
-        expect((JSON.parse(response.body)).count).to eq (users.count - 1)
-      end
-
-      %w(id email).each do |attr|
+      %w(id email created_at updated_at).each do |attr|
         it "contains #{attr}" do
-          arr = JSON.parse(response.body)
-          arr.each_with_index do |a, iter|
-            expect(a["#{attr}"]).to eq users[(iter + 1)]["#{attr}"]
-          end
+          expect(response.body).to be_json_eql(users[1].send(attr.to_sym).to_json).at_path("0/#{attr}")
         end
       end
 
       %w(password encrypted_password).each do |attr|
         it "does not contain #{attr}" do
-          arr = JSON.parse(response.body)
-          arr.each do |a|
-            expect(a.try("#{attr}".to_sym)).to eq nil
-          end
+          expect(response.body).to_not have_json_path("0/#{attr}")
         end
-      end    
+      end
+      it "return array of user objects propper length" do        
+        expect((JSON.parse(response.body)).count).to eq (users.count - 1)
+      end
     end
-  
+
 
   end
 end
