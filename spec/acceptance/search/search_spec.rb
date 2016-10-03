aee650c7bc71c007989970b760de01856ce44829
Anti-k <romanslyzebniy@gmail.com>
2016-10-03 18:16:08 +0300

    specs fixed

--- spec/acceptance/search/search_spec.rb
+++ spec/acceptance/search/search_spec.rb
@@ -1,57 +1,62 @@
 require_relative '../acceptance_helper'
 
-feature 'User searches documents', '
-  In order to find neccessary information
-  As an guest
-  I want to be able to search documents
-' do
-  given!(:user)     { create :user }
-  given!(:question) { create :question, user: user }
-  given!(:answer)   { create :answer,   user: user }
-  given!(:comment)  { create :comment,  commentable: question, user: user }
+feature 'Search' do
+  given!(:user) { create(:user) }
+  given!(:question) { create(:question) }
+  given!(:answer) { create(:answer) }
+  given!(:comment) { create(:comment , body: 'somebody', user: user, commentable: answer) }
+  
 
-  scenario 'Guest try to find User', sphinx: true do
-    search(query: user.email, model: 'Users')
-
-    expect(page).to     have_content "User #{user.email} registered at"
-    expect(page).not_to have_content "asked"
-    expect(page).not_to have_content "answered"
-    expect(page).not_to have_content "commented"
+  background do
+    ThinkingSphinx::Test.index
+    sleep(0.25)
+    visit search_index_path
   end
 
-  scenario 'Guest try to find Question', sphinx: true do
-    search(query: user.email, model: 'Questions')
-
-    expect(page).to     have_content "User #{user.email} asked"
-    expect(page).not_to have_content "registered at"
-    expect(page).not_to have_content "answered"
-    expect(page).not_to have_content "commented"
+  scenario 'Visit search page', :sphinx do
+    expect(current_path).to eq search_path
   end
 
-  scenario 'Guest try to find Answer', sphinx: true do
-    search(query: user.email, model: 'Answers')
-
-    expect(page).to     have_content "User #{user.email} answered"
-    expect(page).not_to have_content "asked"
-    expect(page).not_to have_content "registered at"
-    expect(page).not_to have_content "commented"
+  scenario 'See search query', :sphinx do
+    expect(page).to have_field 'Search'
   end
 
-  scenario 'Guest try to find Comments', sphinx: true do
-    search(query: user.email, model: 'Comments')
+  scenario 'See result with all', :sphinx do
+    select 'All', from: 'model'
+    fill_in 'query', with: ''
+    click_button 'Find'
 
-    expect(page).to     have_content "User #{user.email} commented"
-    expect(page).not_to have_content "asked"
-    expect(page).not_to have_content "answered"
-    expect(page).not_to have_content "registered at"
+    expect(page).to have_content question.title
+    expect(page).to have_content answer.body
+    expect(page).to have_content comment.body
+    expect(page).to have_content user.email
   end
 
-  scenario 'Guest try to find Anywhere', sphinx: true do
-    search(query: user.email, model: 'Anywhere')
+  scenario 'See result with questions', :sphinx do
+    select 'Question', from: 'model'
+    fill_in 'query', with: question.title
+    click_button 'Find'
+    expect(page).to have_content question.title
+  end
 
-    expect(page).to  have_content "User #{user.email} registered at"
-    expect(page).to  have_content "User #{user.email} asked"
-    expect(page).to  have_content "User #{user.email} answered"
-    expect(page).to  have_content "User #{user.email} commented"
+  scenario 'See result with answers', :sphinx do
+    select 'Answer', from: 'model'
+    fill_in 'query', with: answer.body
+    click_button 'Find'
+    expect(page).to have_content answer.body
+  end
+
+  scenario 'See result with comments', :sphinx do
+    select 'Comment', from: 'model'
+    fill_in 'query', with: comment.body
+    click_button 'Find'
+    expect(page).to have_content comment.body
+  end
+
+  scenario 'See result with users', :sphinx do
+    select 'User', from: 'model'
+    fill_in 'query', with: user.email
+    click_button 'Find'
+    expect(page).to have_content user.email
   end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe SearchController, type: :controller do
+RSpec.describe SearchController do
+  describe 'GET #search' do
+    let(:do_request){ get :search, { query: 'test', model: 'All' } }
 
+    it_behaves_like 'Renderable templates', :search
+
+    context 'search' do
+      it 'should receive search' do
+        expect(ThinkingSphinx).to receive(:search).with('test')
+        do_request
+      end
+    end
+  end
 end
